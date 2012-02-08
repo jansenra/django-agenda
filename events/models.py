1df0026f05de8b8e39b8d573f7ae92fda1c2de4e
Richard Jansen <richard.jansen@getlogic.nl>
2012-02-08 11:44:26 +0100

    - Removed site references - Now start date and end date is possible - Added tinymce HTMLField on description

--- events/models.py
+++ events/models.py
@@ -2,28 +2,20 @@ from datetime import datetime, date
 import calendar
 from itertools import groupby
 
-from django.utils.html import conditional_escape as esc
-
 from django.db import models
-
+from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import ugettext
-
 from django.conf import settings
-
 from django.contrib.auth.models import User
-
-from django.contrib.sites.models import Site
-from django.contrib.sites.managers import CurrentSiteManager
-
 from django.contrib.sitemaps import ping_google
-from apps.core.widgets import LocationField
-
+from tinymce.models import HTMLField
 from sorl.thumbnail import ImageField
 
-from django.core.urlresolvers import reverse
+from apps.core.widgets import LocationField
 
 from .managers import EventManager
+
 
 class Location(models.Model):
     class Meta:
@@ -37,54 +29,47 @@ class Location(models.Model):
     title = models.CharField(_('title'), max_length=255)
     slug = models.SlugField(_('slug'), db_index=True)
     location = LocationField(blank=True, max_length=255)
-
     address = models.CharField(_('address'), max_length=255, blank=True)
-
-
-class PublicationManager(CurrentSiteManager):
-    def get_query_set(self):
-        return super(CurrentSiteManager, self).get_query_set().filter(publish=True, publish_date__lte=datetime.now())
 
 
 class Event(models.Model):
     class Meta:
         verbose_name = _('event')
         verbose_name_plural = _('events')
-        ordering = ['-event_date', '-start_time', '-title']
-        get_latest_by = 'event_date'
+        ordering = ['-start_date', '-start_time', '-title']
+        get_latest_by = 'start_date'
         permissions = (("change_author", ugettext("Change author")),)
-        unique_together = ("event_date", "slug")
+        unique_together = ("start_date", "slug")
 
     def __unicode__(self):
-        return _("%(title)s on %(event_date)s") % { 'title'      : self.title,
-                                                    'event_date' : self.event_date }
+        return _("%(title)s on %(start_date)s") % { 'title'      : self.title,
+                                                    'start_date' : self.start_date }
 
     @models.permalink                                               
     def get_absolute_url(self):
         return ('events-detail', (), {
-                  'year'  : self.event_date.year, 
-                  'month' : self.event_date.month, 
-                  'day'   : self.event_date.day, 
+                  'year'  : self.start_date.year,
+                  'month' : self.start_date.month,
+                  'day'   : self.start_date.day,
                   'slug'  : self.slug })
 
     objects = EventManager()
-    on_site = CurrentSiteManager()
-    published = PublicationManager()
 
     # Core fields
     title = models.CharField(_('title'), max_length=255)
     slug = models.SlugField(_('slug'), db_index=True)
     
-    event_date = models.DateField(_('date'))
-    
+    start_date = models.DateField(_('start date'))
     start_time = models.TimeField(_('start time'), blank=True, null=True)
+
+    end_date = models.DateField(_('end date'))
     end_time = models.TimeField(_('end time'), blank=True, null=True)
     
     location = models.ForeignKey(Location, blank=True, null=True)
 
     image = ImageField(verbose_name=_('image'), upload_to='event_images', blank=True, null=True)
 
-    description = models.TextField(_('description'))
+    description = HTMLField(_('description'))
 
     calendar = models.ForeignKey("Calendar", blank=True, null=True, related_name='events')
 
@@ -92,15 +77,13 @@ class Event(models.Model):
     add_date = models.DateTimeField(_('add date'),auto_now_add=True)
     mod_date = models.DateTimeField(_('modification date'), auto_now=True)
     
-    author = models.ForeignKey(User, verbose_name=_('author'), db_index=True, blank=True, null=True)
+    created_by = models.ForeignKey(User, verbose_name=_('created by'), db_index=True, blank=True, null=True)
 
     publish_date = models.DateTimeField(_('publication date'), default=datetime.now())
     publish = models.BooleanField(_('publish'), default=True)
     
     allow_comments = models.BooleanField(_('Allow comments'), default=True)
 
-    sites = models.ManyToManyField(Site)
-    
     def save(self):
         super(Event, self).save()
         if not settings.DEBUG:
@@ -113,7 +96,6 @@ class Event(models.Model):
 
 class TitleAbstractBase(models.Model):
     """ Abstract base class adding a title field. """
-
     class Meta:
         abstract = True
 
@@ -125,20 +107,18 @@ class TitleAbstractBase(models.Model):
 
 class EventImage(TitleAbstractBase):
     """ Image related to an event. """
-    
     event = models.ForeignKey(Event)
     image = ImageField(verbose_name=_('image'), upload_to='event_images')
 
 
 class EventFile(TitleAbstractBase):
     """ File related to an event. """
-    
     event = models.ForeignKey(Event)
     file = models.FileField(verbose_name=_('file'), upload_to='event_files')
 
 
 class Calendar(models.Model):
-    name = models.CharField(_('name'), max_length=100, blank=True, null=True)
+    name = models.CharField(_('name'), max_length=100, blank=False, null=False)
 
     class Meta:
         verbose_name = _('calendar')
@@ -147,14 +127,12 @@ class Calendar(models.Model):
     def __unicode__(self):
         if self.name:
             return self.name
-        return _("Unnamed Calendar")
 
 
 class EventCalendar(calendar.HTMLCalendar):
     """
     Event calendar is a basic calendar made with HTMLCalendar module.
     """
-
     def __init__(self, events, *args, **kwargs):
         self.events = self.group_by_day(events)
         super(EventCalendar, self).__init__(*args, **kwargs)
@@ -176,7 +154,7 @@ class EventCalendar(calendar.HTMLCalendar):
         return super(EventCalendar, self).formatmonth(year, month)
 
     def group_by_day(self, events):
-        field = lambda event: event.event_date.day
+        field = lambda event: event.start_date.day
         return dict(
             [(day, list(items)) for day, items in groupby(events, field)]
         )
