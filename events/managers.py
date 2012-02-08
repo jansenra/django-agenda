9a27dd8946bfd9781e03d57a584aabddabcb990c
Richard Jansen <richard.jansen@getlogic.nl>
2012-02-08 11:45:35 +0100

    Changes to reflect start and end date

--- events/managers.py
+++ events/managers.py
@@ -5,7 +5,7 @@ class EventManager(Manager):
 
     def get_upcomming(self):
         now = datetime.now()
-        return self.get_query_set().filter(event_date__gte=now - timedelta(days=1))
+        return self.get_query_set().filter(start_date__gte=now - timedelta(days=1))
 
     def get_by_date(self, year=None, month=None, day=None):
         events = self.get_query_set()
@@ -13,14 +13,14 @@ class EventManager(Manager):
         if not year:
             return events
 
-        events = events.filter(event_date__year=year)
+        events = events.filter(start_date__year=year)
 
         if not month:
             return events
 
-        events = events.filter(event_date__month=month)
+        events = events.filter(start_date__month=month)
 
         if not day:
             return events
 
-        return events.filter(event_date__day=day)
+        return events.filter(start_date__day=day)
