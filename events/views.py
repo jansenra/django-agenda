33eccc740e562750f82b08497f98c61dd3f0ede6
Richard Jansen <richard.jansen@getlogic.nl>
2012-02-09 11:52:33 +0100

    Added view for creating an RSVP. Overview of events shows this month and next month in sidebar. Detail view shows list of attendees.

--- events/views.py
+++ events/views.py
@@ -1,9 +1,14 @@
 import datetime
+from django.http import HttpResponseRedirect
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.shortcuts import get_object_or_404
 
-from .models import Event, EventCalendar
+from .models import Event, EventUsers
+from .forms import RsvpForm
+from .html_calendar import EventCalendar
 
 from apps.replies.views import ReplyMixin
 
@@ -20,6 +25,16 @@ class Detail(DetailView, ReplyMixin):
     def get_context_data(self, **kwargs):
         context = super(Detail, self).get_context_data(**kwargs)
         context['form'] = self.get_form(self.get_form_class())
+        context['attendees'] = EventUsers.objects.filter(event=self.object, rsvp="going")
+        user_rsvp = ""
+        try:
+            rsvp = EventUsers.objects.get(event=self.object, user=self.request.user)
+            user_rsvp = rsvp.rsvp
+        except EventUsers.DoesNotExist:
+            pass
+
+        context['user_rsvp'] = user_rsvp
+
         return context
 
 
@@ -35,6 +50,24 @@ class Index(ArchiveMixin, ListView):
     def get_queryset(self):
         return Event.objects.get_upcomming()
 
+    def get_calendar(self, date_in):
+        events = Event.objects.get_by_date(year=date_in.year, month=date_in.month)
+        return EventCalendar(events).formatmonth(date_in.year, date_in.month)
+
+    def get_context_data(self, **kwargs):
+        today = datetime.date.today()
+        this_month = datetime.date(today.year, today.month, 1)
+
+        try:
+            next_month = this_month.replace(month=this_month.month+1)
+        except ValueError:
+            next_month = this_month.replace(year=this_month.year+1, month=1)
+
+        context = super(Index, self).get_context_data(**kwargs)
+        context['calendar_this_month'] = self.get_calendar(this_month)
+        context['calendar_next_month'] = self.get_calendar(next_month)
+
+        return context
 
 class Day(ArchiveMixin, ListView):
     def get_queryset(self):
@@ -75,3 +108,44 @@ class Widget(TemplateView):
         context['calendar'] = EventCalendar(events).formatmonth(today.year, today.month)
 
         return context
+
+
+class RsvpCreate(CreateView):
+    model = EventUsers
+    _event = None
+    template_name = "attendees/create.html"
+    form_class = RsvpForm
+
+    @property
+    def event(self):
+        if not self._event:
+            self._event = get_object_or_404(Event, pk=self.request.POST.get('event_id'))
+        return self._event
+
+    def form_valid(self, form):
+        rsvp = self.request.POST.get('rsvp')
+        if rsvp == 'remove':
+            try:
+                object = EventUsers.objects.get(event=self.event, user=self.request.user)
+                object.delete()
+            except EventUsers.DoesNotExist:
+                pass
+            return HttpResponseRedirect(self.get_success_url())
+
+        # check if a record already exists
+        try:
+            object = EventUsers.objects.get(event=self.event, user=self.request.user)
+            object.rsvp = rsvp
+            object.save()
+            return HttpResponseRedirect(self.get_success_url())
+        except EventUsers.DoesNotExist:
+            pass
+
+        object = form.save(commit=False)
+        object.event = self.event
+        object.user = self.request.user
+        object.save()
+        return super(RsvpCreate, self).form_valid(form)
+
+    def get_success_url(self):
+        return self.event.get_absolute_url()
