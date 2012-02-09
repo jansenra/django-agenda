f61879cc426d6bad6127a553e4de933ac76f294a
Richard Jansen <richard.jansen@getlogic.nl>
2012-02-09 11:56:36 +0100

    Changed selectors somewhat. Added handlers for RSVP actions.

--- events/static/events/js/functions.js
+++ events/static/events/js/functions.js
@@ -1,8 +1,14 @@
 $(function() {
-    $('div.events-calendar a.view_events').click(function(e) {
+    $('a.view_events').click(function(e) {
         e.stopPropagation();
         e.preventDefault();
-        console.log($(this).attr('href'));
-        $('div.events div.events-outline').load($(this).attr('href'));
+        $('div.events-outline').load($(this).attr('href'));
     })
+
+    $('div.event-detail a.rsvp').click(function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        $('#rsvp_form input[name=rsvp]').val($(this).attr('rsvp'));
+        $('#rsvp_form').submit();
+    });
 });
